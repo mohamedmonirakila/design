34aa767ad53eaeccfeb367974d8dfe215d103848
mohamedmonirakila <mohamedmonirakila@gmail.com>
2025-01-25 17:08:21 +0200

    finish navbar

--- website-vite/src/App.jsx
+++ website-vite/src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import Home from "./components/Home";
 import Feature from "./components/Feature";
 import Projects from "./components/Porjects";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Home />
       <Feature />
       <Projects />
--- website-vite/src/components/Home.jsx
+++ website-vite/src/components/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./Navbar";
 import Hero from "./Hero";
-
 
 export default function Home() {
   return (
     <div>
       <div className="home">
         <div className="content">
-          <Navbar />
           <Hero />
         </div>
       </div>
