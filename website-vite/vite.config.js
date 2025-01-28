6ef974104a0dea8bae7551440d778059d2184f24
mohamedmonirakila <mohamedmonirakila@gmail.com>
2025-01-29 00:45:22 +0200

    finish pages

--- website-vite/vite.config.js
+++ website-vite/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Use '/' if deploying to the root domain
+  build: {
+    outDir: "dist", // Vercel uses this for deployment
+  },
+});
